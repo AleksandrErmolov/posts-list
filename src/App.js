9e06653e73850543148f97718f8756c27465b0d1
unknown <ermolov1337@gmail.com>
2021-11-24 12:13:06 +0300

    add input and button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { useState } from "react"
-import Counter from "./components/Counter";
 import '../src/styles/App.css'
-import Postitem from "./components/Postitem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 function App() {
+
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript 1', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
+  
+  ])
+
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description' },
+    { id: 2, title: 'Python 1', body: 'Description' },
+    { id: 3, title: 'Python 2', body: 'Description' },
+
+  ])
+
 
 
   return (
     <div className="App">
-
-      <Postitem post={{id:1, title:'JavaScript', body:'Description'}}/>
-      <Postitem />
-      <Postitem />
-      <Postitem />
-      <Postitem />
+      <form>
+        <MyInput type="text" placeholder={'Название поста'} />
+        <MyInput type="text" placeholder={'Описание поста'} />
+        <MyButton>Создать пост</MyButton>
+        </form>
+      <PostList posts={posts2} title="Посты про JS" />
 
     </div>
   );
---
+++ src/components/PostList.jsx
@@ -0,0 +1,16 @@
+import Postitem from "./Postitem";
+
+
+function PostList({posts, title}) {
+
+    return (
+
+        <div>
+            <h1 style={{ textAlign: 'center' }}>{title}</h1>
+            {posts.map((post) => <Postitem post={post} key={post.id} />)}
+
+        </div>
+    )
+}
+
+export default PostList;
