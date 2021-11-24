6b38029840c936517ea8bbdd9b2a1679d5115e3b
unknown <ermolov1337@gmail.com>
2021-11-24 13:28:18 +0300

    add distr

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
 import React, { useState } from "react"
 import '../src/styles/App.css'
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+
+
 function App() {
 
   const [posts, setPosts] = useState([
     { id: 1, title: 'JavaScript 1', body: 'Description' },
     { id: 2, title: 'JavaScript 2', body: 'Description' },
-    { id: 3, title: 'JavaScript 3', body: 'Description' },
-  
+    { id: 3, title: 'JavaScript 3', body: 'Description' },  
   ])
 
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-
-  const addNewPost = (e) => {
-  e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body,
-    }
-    setPosts([...posts, newPost])
-    setTitle('')
-    setBody('')
+  const createPost = (newPost) => {
+  setPosts([...posts, newPost])
   }
+  
 
   return (
     <div className="App">
-      <form>
-        <MyInput
-          value={title}
-          onChange={e => setTitle(e.target.value) }
-          type="text"
-          placeholder={'Название поста'}
-        />
-        <MyInput
-          value={body}
-          onChange={e => setBody(e.target.value)}
-          type="text"
-          placeholder={'Описание поста'}
-        />
-        <MyButton type="submit" onClick={addNewPost}> Создать пост</MyButton>
-        </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title="Посты про JS" />
 
     </div>
